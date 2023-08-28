02d49b4618e3b9c7ef24987c68638179275799a9
Phummy_phum <phumrapee.phum@gmail.com>
2023-08-28 16:12:17 +0700

    success

--- src/app/page.js
+++ src/app/page.js
@@ -50,7 +50,7 @@ export default function FoodReviewPage() {
         position="center"
         color="orange"
       ></Pagination>
-      <Text align="center" color="dimmed" my="sm">
+      <Text align="center" color="dimmed" my="sm" mt="sm">
         Copyright © 2023 Phumrapee Tapwong 650610799
       </Text>
     </Container>
