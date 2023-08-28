69050de5612677486ea8ddeba087bbdb1f87c8f7
Phummy_phum <phumrapee.phum@gmail.com>
2023-08-28 15:49:07 +0700

    test1

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,33 @@
 "use client";
-import { Container, Text, Title } from "@mantine/core";
+import {
+  Button,
+  Container,
+  Rating,
+  Text,
+  Textarea,
+  Title,
+} from "@mantine/core";
 
 export default function FoodReviewPage() {
   return (
     <Container size="600px">
       <Title order={2}>Food Review 🍕</Title>
+      <Title order={4} mt="sm">
+        Your rating
+      </Title>
+      <Rating size="lg" />
+      <Textarea
+        label="Your review"
+        placeholder="Do you enjoy eating?"
+        mt="xs"
+        minRows={3}
+      />
+      <Button mt="xs" color="orange">
+        Submit Review
+      </Button>
 
       <Text align="center" color="dimmed" my="sm">
-        Copyright © 2023 Chayanin Suatap 650610560
+        Copyright © 2023 Phumrapee Tapwong 650610799
       </Text>
     </Container>
   );
